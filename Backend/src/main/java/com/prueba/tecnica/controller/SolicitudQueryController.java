b336b7c66015bdbe2b598dac11bba6950ede7ebf
lanza11 <mateo.vasquezg@udea.edu.co>
2026-01-08 10:09:04 -0500

    Organizando configuraciones basicas para deplegar en ambiente AWS

--- Backend/src/main/java/com/prueba/tecnica/controller/SolicitudQueryController.java
+++ Backend/src/main/java/com/prueba/tecnica/controller/SolicitudQueryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Listar")
+@RequestMapping("/api/Listar")
 @RequiredArgsConstructor
 public class SolicitudQueryController {
 
